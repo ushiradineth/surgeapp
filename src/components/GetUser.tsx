88108f283211b019aed8a1351f70cbc64362e268
Ushira Dineth <ushiradineth@gmail.com>
2023-01-23 18:27:55 +0530

    added global user state

---
+++ src/components/GetUser.tsx
@@ -0,0 +1,35 @@
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { api } from "../utils/api";
+import Spinner from "./Spinner";
+
+interface itemType {
+  user: any;
+  setUser: (params: any) => any;
+  status: any;
+  setStatus: (params: any) => any;
+  enabled?: boolean;
+}
+
+const GetUser = (props: itemType) => {
+  const { data: session, status } = useSession();
+  const user = api.user.getUserById.useQuery({ id: session?.user?.id || "" }, { enabled: status === "authenticated" && props.enabled, retry: false, refetchOnWindowFocus: false });
+
+  useEffect(() => {
+    if (user.isSuccess && props.user !== user) props.setUser(user);
+    if (status !== props.status && status === "loading" && user.isLoading) props.setStatus(status);
+    if (status !== props.status && status === "authenticated" && user.isSuccess) props.setStatus(status);
+    if (status !== props.status && status === "unauthenticated") props.setStatus(status);
+  }, [user.isSuccess, status]);
+
+  useEffect(() => {
+    if (user.isSuccess && user.isFetched && props.user != null ? props.user.data !== user.data : false) {
+      props.setUser(user);
+    }
+  }, [user]);
+
+  if ((status === "loading" || props.status === "loading" || user.isLoading) && props.enabled) return <Spinner />;
+  return <></>;
+};
+
+export default GetUser;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,43 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-
 import { api } from "../utils/api";
-
 import "../styles/globals.css";
+import NavBar from "../components/NavBar";
+import React, { useState, createContext } from "react";
+import { type MemoType } from "../types/types";
+import { env } from "../env/client.mjs";
+import GetUser from "../components/GetUser";
+import { createClient } from "@supabase/supabase-js";
+
+export const DataContext = createContext<MemoType | undefined | null>(null);
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+  const [user, setUser] = useState(null);
+  const [create, setCreate] = useState(false);
+  const [status, setStatus] = useState("");
+  const supabase = createClient("https://" + env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY);
+
+  const value = React.useMemo(() => {
+    return {
+      user,
+      supabase,
+    };
+  }, [user, supabase]);
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {status === "authenticated" || status === "unauthenticated" ? (
+        <>
+          <GetUser user={user} setUser={setUser} status={status} setStatus={setStatus} enabled={false} />
+          <DataContext.Provider value={value}>
+            <NavBar user={user} create={create} setCreate={setCreate} />
+            <Component {...pageProps} />
+          </DataContext.Provider>
+        </>
+      ) : (
+        <GetUser user={user} setUser={setUser} status={status} setStatus={setStatus} enabled={true} />
+      )}
     </SessionProvider>
   );
 };
