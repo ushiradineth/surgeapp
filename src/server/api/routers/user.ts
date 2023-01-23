312f8fc1faa0abe32dd7ce90f0a9c42eff1e1490
Ushira Dineth <ushiradineth@gmail.com>
2023-01-23 17:23:04 +0530

    added getUser query

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,19 +3,25 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const userRouter = createTRPCRouter({
-  hello: publicProcedure
-    .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
-      return {
-        greeting: `Hello ${input.text}`,
-      };
-    }),
+  hello: publicProcedure.input(z.object({ text: z.string() })).query(({ input }) => {
+    return {
+      greeting: `Hello ${input.text}`,
+    };
+  }),
 
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
 
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can now see this secret message!";
+  getUser: protectedProcedure.input(z.object({ handle: z.string() })).query(({ input, ctx }) => {
+    return ctx.prisma.user.findFirstOrThrow({
+      where: {
+        handle: input.handle
+      }, 
+      include: {
+        posts: true,
+        likes: true
+      }
+    });
   }),
 });
