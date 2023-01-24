598e62b18325c6afa8820af1fcf4ba739debe591
Ushira Dineth <ushiradineth@gmail.com>
2023-01-24 21:46:34 +0530

    fixed post alignment

--- src/pages/post/[post].tsx
+++ src/pages/post/[post].tsx
@@ -2,11 +2,14 @@ import { useRouter } from "next/router";
 import React from "react";
 import PostItem from "../../components/PostItem";
 
-
 const Post = () => {
   const router = useRouter();
 
-  return <PostItem postID={router.query.post as string} />
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-zinc-700">
+      <PostItem postID={router.query.post as string} />
+    </div>
+  );
 };
 
 export default Post;
