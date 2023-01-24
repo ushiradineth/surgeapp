38b49d8fa28baa48b33540ab4c7820927884495b
Ushira Dineth <ushiradineth@gmail.com>
2023-01-24 14:42:32 +0530

    added more queries

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -14,4 +14,49 @@ export const postRouter = createTRPCRouter({
       },
     });
   }),
+
+  getPost: publicProcedure.input(z.object({ id: z.string() })).query(({ input, ctx }) => {
+    return ctx.prisma.post.findFirst({
+      where: {
+        id: input.id,
+      },
+      include: {
+        user: true,
+        likes: true,
+      },
+    });
+  }),
+
+  deletePost: protectedProcedure.input(z.object({ id: z.string() })).mutation(({ input, ctx }) => {
+    return ctx.prisma.post.delete({
+      where: { id: input.id },
+    });
+  }),
+
+  likePost: protectedProcedure.input(z.object({ postid: z.string(), postOwnerid: z.string(), userid: z.string() })).mutation(async ({ input, ctx }) => {
+    const q1 = await ctx.prisma.post.update({
+      where: { id: input.postid },
+      data: { likes: { connect: { id: input.userid } } },
+    });
+
+    const q2 = await ctx.prisma.user.update({
+      where: { id: input.userid },
+      data: { likes: { connect: { id: input.postid } } },
+    });
+
+    return { q1, q2 };
+  }),
+
+  unlikePost: protectedProcedure.input(z.object({ postid: z.string(), userid: z.string() })).mutation(async ({ input, ctx }) => {
+    const q1 = await ctx.prisma.post.update({
+      where: { id: input.postid },
+      data: { likes: { disconnect: { id: input.userid } } },
+    });
+    const q2 = await ctx.prisma.user.update({
+      where: { id: input.userid },
+      data: { likes: { disconnect: { id: input.postid } } },
+    });
+
+    return { q1, q2 };
+  }),
 });
