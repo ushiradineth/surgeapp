82a69310d8e85580c1530903ecced1197cb36758
Ushira Dineth <ushiradineth@gmail.com>
2023-01-24 21:47:06 +0530

    added posts timeline to home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -10,20 +10,22 @@ import Head from "next/head";
 const Home = () => {
   const posts = api.post.getAllPosts.useQuery(undefined, { retry: false, refetchOnWindowFocus: false });
 
-  return(
+  return (
     <>
-        <Head>
-          <title>Home</title>
-          <meta name="description" content="SurgeApp by Ushira Dineth" />
-          <link rel="icon" href={"/favicon.ico"} />
-        </Head>
-      {posts.data?.map((post, index) => {
-        return (
-          <div key={index} >
-            <PostItem key={index} post={{ data: post, isLoading: posts.isLoading, isError: posts.isError, isSuccess: posts.isSuccess }} />
-          </div>
-        );
-      })}
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="SurgeApp by Ushira Dineth" />
+        <link rel="icon" href={"/favicon.ico"} />
+      </Head>
+      <main className="pb-4">
+        {posts.data?.map((post, index) => {
+          return (
+            <div key={index}>
+              <PostItem key={index} post={{ data: post, isLoading: posts.isLoading, isError: posts.isError, isSuccess: posts.isSuccess }} />
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 };
