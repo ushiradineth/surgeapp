e55a901787fce88e4bd6c9128196f5e447beca68
Ushira Dineth <ushiradineth@gmail.com>
2023-01-24 15:25:15 +0530

    removed log

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -4,8 +4,6 @@ import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
 
 export const postRouter = createTRPCRouter({
   setPost: protectedProcedure.input(z.object({ id: z.string(), links: z.array(z.string()), caption: z.string().nullish() })).mutation(({ input, ctx }) => {
-    input.links.forEach((e) => console.log(e));
-
     return ctx.prisma.post.create({
       data: {
         user: { connect: { id: input.id } },
