d1137c436b8f76a44fbb432f20b72c404ba2e486
Ushira Dineth <ushiradineth@gmail.com>
2023-01-24 11:45:45 +0530

    deleted component

--- src/components/Create.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Create = () => {
-  return <div className="h-screen w-screen flex items-center justify-center text-white ">Create</div>;
-};
-
-export default Create;
