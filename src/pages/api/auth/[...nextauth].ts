45f7b3058a0c8ca8a16338105070e6c418fba52d
Ushira Dineth <ushiradineth@gmail.com>
2023-01-25 16:01:43 +0530

    added credential login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,36 +1,80 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
 import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db";
+import { verify } from "argon2";
+
+type Student2 = {
+  [key: string]: any;
+  name: string;
+};
 
 export const authOptions: NextAuthOptions = {
-  // Include user.id on session
+  session: {
+    strategy: "jwt",
+  },
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.user = user;
       }
-      session = {
-        ...session,
-        user: {
-          id: user.id,
-          handle: user.handle,
-          email: user.email,
-          name: user.name,
-          image: user.image,
-        },
-      };
-      return session;
+      return Promise.resolve(token as Student2);
+    },
+    session({ session, token }) {
+      if (token.user) {
+        const t = token.user as {
+          id: string;
+          handle: string;
+        };
+
+        session.user = {
+          id: t.id,
+          handle: t.handle,
+          email: token.email,
+          image: token.picture,
+          name: token.name,
+        };
+      }
+      return Promise.resolve(session);
     },
   },
-  // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user: any = await prisma.user.findFirst({
+          where: { email: credentials?.email },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const isValidPassword = await verify(user.password || "", credentials?.password || "");
+
+        if (!isValidPassword) {
+          return null;
+        }
+
+        return {
+          id: user.id,
+          email: user.email || "",
+          name: user.name || "",
+          image: user.image || "",
+          handle: user.handle,
+        };
+      },
     }),
   ],
 };
