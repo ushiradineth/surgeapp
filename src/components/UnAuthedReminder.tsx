7d3cfa70d73ee319791c6e8c41b4a6530c426470
Ushira Dineth <ushiradineth@gmail.com>
2023-01-26 12:30:31 +0530

    fixed text visibility

--- src/components/UnAuthedReminder.tsx
+++ src/components/UnAuthedReminder.tsx
@@ -5,9 +5,9 @@ export const UnAuthedReminder = () => {
   const router = useRouter();
   
   return (
-    <div className={"fixed bottom-0 left-0 flex h-12 w-screen items-center justify-center gap-2"}>
+    <div className={"fixed bottom-0 left-0 flex h-12 w-screen items-center justify-center font-medium  gap-2 bg-zinc-500"}>
       Sign in to SurgeApp to see more!
-      <button className={"rounded-full px-4 py-2 font-semibold no-underline transition"} onClick={() => router.push("/")}>
+      <button className={"rounded-full px-4 py-2 font-semibold no-underline transition bg-zinc-200"} onClick={() => router.push("/")}>
         Sign in
       </button>
     </div>
